d2ce53ceb9364f7f72c62e7489fbfe4da4ec0a5d
sayawrt <rifkitaufik01@gmail.com>
2023-03-09 02:10:23 +0700

    belajar react

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import {
+    IconBrandFacebook,
+    IconBrandGithub,
+    IconBrandTwitter,
+} from '@tabler/icons-react';
+import clsx from 'clsx';
+
+export default function App() {
+    return (
+        <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
+            <div className={'flex gap-2'}>
+                <Button
+                    onClick={() => console.log('hai ini aku')}
+                    type='submit'
+                >
+                    <IconBrandFacebook />
+                    Register
+                </Button>
+                <Button
+                    className='bg-sky-600'
+                    onClick={() => console.log('Ini aku Wahyu')}
+                    type='button'
+                >
+                    <IconBrandTwitter />
+                    Login
+                </Button>
+                <Button
+                    className='bg-black'
+                    onClick={() => console.log('Ini aku Wahyu')}
+                    type='button'
+                >
+                    <IconBrandGithub />
+                    Login
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+function Button(props) {
+    const {
+        className = 'bg-blue-600',
+        children,
+        text,
+        type = 'submit',
+    } = props;
+    return (
+        <button
+            {...props}
+            type={type}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
+            )}
+        >
+            {text || children}
+        </button>
+    );
+}
