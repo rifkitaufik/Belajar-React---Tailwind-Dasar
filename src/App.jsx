843654c1249ca54d36859b4aa58b387a2d7004b1
sayawrt <rifkitaufik01@gmail.com>
2023-03-09 11:56:09 +0700

    belajar nested fungsi dan component

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,29 @@
-import {
-    IconBrandFacebook,
-    IconBrandGithub,
-    IconBrandTwitter,
-} from '@tabler/icons-react';
-import clsx from 'clsx';
+import Button from './components/Button';
+import Card from './components/Card';
 
-export default function App() {
+function App() {
     return (
-        <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
-            <div className={'flex gap-2'}>
-                <Button
-                    onClick={() => console.log('hai ini aku kamu juga')}
-                    type='submit'
-                >
-                    <IconBrandFacebook />
-                    Register
-                </Button>
-                <Button
-                    className='bg-sky-600'
-                    onClick={() => console.log('Ini aku Wahyu')}
-                    type='button'
-                >
-                    <IconBrandTwitter />
-                    Login
-                </Button>
-                <Button
-                    className='bg-black'
-                    onClick={() => console.log('Ini aku Wahyu')}
-                    type='button'
-                >
-                    <IconBrandGithub />
-                    Login
-                </Button>
+        <div
+            className={
+                'bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'
+            }
+        >
+            <div className={'max-w-xl w-full'}>
+                <Card>
+                    <Card.Title>Hello Taufik</Card.Title>
+                    <Card.Body>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Quisquam soluta doloribus aliquid velit dolores a
+                        unde aut sed, officia beatae nulla sapiente iste omnis
+                        ducimus minima neque excepturi atque quam?
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button>Register</Button>
+                    </Card.Footer>
+                </Card>
             </div>
         </div>
     );
 }
 
-function Button(props) {
-    const {
-        className = 'bg-blue-600',
-        children,
-        text,
-        type = 'submit',
-    } = props;
-    return (
-        <button
-            {...props}
-            type={type}
-            className={clsx(
-                className,
-                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
-            )}
-        >
-            {text || children}
-        </button>
-    );
-}
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+
+function Button(props) {
+    const {
+        className = 'bg-blue-600',
+        children,
+        text,
+        type = 'submit',
+    } = props;
+    return (
+        <button
+            {...props}
+            type={type}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
+            )}
+        >
+            {text || children}
+        </button>
+    );
+}
+
+export default Button;
